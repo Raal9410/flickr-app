f8419d8618c22b208f4544b43a8f8f28b916ac5e
Raul Hernandez <raulhernandez@Rauls-MacBook-Pro.local>
2020-07-25 18:21:02 -0500

    Add filtered route with query

--- backend/routes/feed.js
+++ backend/routes/feed.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
-const {publicPhotos} = require('../controllers/flickrController')
+const {publicPhotos, getTagPhotos} = require('../controllers/flickrController')
 
 router.get('/public', publicPhotos)
+
+router.get('/filtered', getTagPhotos)
 
 module.exports = router
