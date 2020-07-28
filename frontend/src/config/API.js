d83cba7f68d4de742db562734a223154255aa3b6
Raul Hernandez <raulhernandez@Rauls-MacBook-Pro.local>
2020-07-27 20:35:02 -0500

    Add Context

--- frontend/src/config/API.js
+++ frontend/src/config/API.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:3500/flickr'
-})
+  baseURL: 'http://localhost:3500/flickr',
+});
 
 export default API;
---
+++ frontend/src/context/context.js
@@ -0,0 +1,50 @@
+import { useState, createContext } from 'react';
+import API from '../config/API';
+
+const useFlickrApi = () => {
+  const [state, setState] = useState({
+    loading: false,
+    error: null,
+    images: [],
+  });
+
+  const fetchPublicFeed = async () => {
+    setState(prevState => ({
+      ...prevState,
+      loading: true,
+    }));
+
+    const {
+      data: { results },
+    } = await API.get('/public/');
+
+    setState(prevState => ({
+      ...prevState,
+      loading: false,
+      images: results,
+    }));
+  };
+
+  const fetchTagFeed = async tag => {
+    setState(prevState => ({
+      ...prevState,
+      loading: true,
+    }));
+
+    const {
+      data: { results },
+    } = await API.get(`/filtered?tag=${tag}`);
+
+    setState(prevState => ({
+      ...prevState,
+      loading: false,
+      images: results,
+    }));
+  };
+
+  return { ...state, fetchPublicFeed, fetchTagFeed };
+};
+
+const Context = createContext(null);
+
+export { useFlickrApi, Context };
