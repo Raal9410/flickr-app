a42aa6c0203092831ad88c77bbfaa0a824503791
Raul Hernandez <raulhernandez@Rauls-MacBook-Pro.local>
2020-07-28 12:31:47 -0500

    Add CORS config to app.js

--- backend/app.js
+++ backend/app.js
@@ -5,10 +5,18 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const flickrRouter = require('./routes/feed');
 
 const app = express();
+
+app.use(
+  cors({
+    credentials: false,
+    origin: 'http://localhost:3000'
+  })
+);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
