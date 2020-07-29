85ccd5094292daa85d3559def18527c5a1dd80ba
Raul Hernandez <raulhernandez@Rauls-MacBook-Pro.local>
2020-07-29 12:15:59 -0500

    Fix dashboard scale

--- frontend/src/components/Dashboard/styles.js
+++ frontend/src/components/Dashboard/styles.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 const Container = styled.div `
-    width: 99vw;
-    height: 99vh;
+    width: 98vw;
+    height: 98vh;
     display: grid;
     grid-template-columns: repeat(auto-fill, minmax(300px, 2fr));
     grid-gap: 1rem;
