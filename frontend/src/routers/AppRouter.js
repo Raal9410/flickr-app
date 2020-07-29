e71148aafcb3b4146f1b7eea3a2a4621978e923e
Raul Hernandez <raulhernandez@Rauls-MacBook-Pro.local>
2020-07-29 18:05:45 -0500

    Code format

--- frontend/src/routers/AppRouter.js
+++ frontend/src/routers/AppRouter.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
-import { useFlickrApi, Context} from '../context/context'
-import Dashboard from '../components/Dashboard/Dashboard'
-import Navbar from '../components/Navbar/Navbar'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { useFlickrApi, Context } from '../context/context';
+import Dashboard from '../components/Dashboard/Dashboard';
+import Navbar from '../components/Navbar/Navbar';
 
 const AppRouter = () => {
-    const data = useFlickrApi()
-    return (
-        <Context.Provider value={data}>
-            <BrowserRouter>
-                <Navbar/>
-                <Switch>
-                    <Route path="/" component={Dashboard} exact={true}/>
-                </Switch>
-            </BrowserRouter>
-        </Context.Provider>
-    )
-}
+  const data = useFlickrApi();
+  return (
+    <Context.Provider value={data}>
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route path="/" component={Dashboard} exact />
+        </Switch>
+      </BrowserRouter>
+    </Context.Provider>
+  );
+};
 
-export default AppRouter
+export default AppRouter;
